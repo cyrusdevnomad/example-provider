ead680d02bf57ce63f256f3c20e8318bda2e5cfd
cyrus devnomad <cyrus.devnomad@gmail.com>
2021-12-17 16:41:15 +0100

    test

--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -5,8 +5,11 @@ class ProductRepository {
     constructor() {
         this.products = new Map([
             ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green")],
+            //["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
             ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")],
+            //["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
             ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "yellow")],
+            //["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")]
         ]);
     }
 
