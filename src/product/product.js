f49bff3436d0ded7e1f1199631fc5cd2d8beb981
cyrus devnomad <cyrus.devnomad@gmail.com>
2021-12-17 18:33:01 +0100

    test

--- src/product/product.js
+++ src/product/product.js
@@ -1,11 +1,11 @@
 class Product {
-    //constructor(id, type, name, version, color) {
-    constructor(id, type, name, version) {
+    constructor(id, type, name, version, color) {
+    //constructor(id, type, name, version) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
-	    // this.color = color;
+	    this.color = color;
     }
 }
 
