ad11014fd8708cbc339f72e50c11939d8438c893
cyrus devnomad <cyrus.devnomad@gmail.com>
2021-12-27 16:51:50 +0100

    test

--- src/product/product.js
+++ src/product/product.js
@@ -1,12 +1,12 @@
 class Product {
-    constructor(id, type, name, version, color, newField) {
+    constructor(id, type, name, version, color, price) {
     //constructor(id, type, name, version) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
 	    this.color = color;
-        this.newField = newField;
+        this.price = price;
     }
 }
 
--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -3,7 +3,7 @@ const { Verifier } = require('@pact-foundation/pact');
 const controller = require('./product.controller');
 const Product = require('./product');
 
-// Setup provider server to verify
+// Setup provider server to verify 
 const app = require('express')();
 const authMiddleware = require('../middleware/auth.middleware');
 app.use(authMiddleware);
@@ -48,21 +48,21 @@ describe("Pact Verification", () => {
     const stateHandlers = {
       "products exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green", 2)],
+          ["10", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green", "11")],
           //["10", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red", 22)]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red", "11")]
           //["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
         ]);
       },
       "products exist": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red", 22)]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red", "11")]
           //["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
         ]);
       },
       "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red", 22)]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red", "11")]
           //["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
         ]);
       },
